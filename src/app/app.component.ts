82d00d8d847746c73700a1763f32e3642bd5740a
abhijith9898 <abhijith.rajeev9898@gmail.com>
2022-11-24 12:24:58 +0530

    base project code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,100 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PrimeNGConfig } from 'primeng/api';
+import { MessageService,ConfirmationService } from 'primeng/api';
+import { User } from '../app/model';
+import { UserServiceService } from '..//app/user-service.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers: [MessageService,ConfirmationService]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-user-crud';
+  userData: User[];
+  currentUserId: any;
+  user: User = {};
+  showAddForm:boolean = false;
+  constructor(private primengConfig: PrimeNGConfig, private messageService: MessageService, private userServiceService:UserServiceService,private confirmationService:ConfirmationService) { }
+
+  ngOnInit() {
+    this.primengConfig.ripple = true;
+    this.getUsersList();
+  }
+
+  openAddNew(){
+    this.user = {};
+    this.showAddForm = true;
+  }
+
+  save(){
+    if(!this.user.id){
+      console.log('payload',this.user);
+      if (this.user.name == undefined || this.user.email == undefined || this.user.gender == undefined || this.user.status == undefined) {
+        this.messageService.add({ severity: 'warn', detail: 'Enter all User details'});      }
+      else {
+        this.userServiceService.createUser(this.user).subscribe((result) => {
+          console.log("user created", result);
+          this.showAddForm = false;
+          this.messageService.add({ severity: 'success', detail: 'User Added Successfully'});
+          this.getUsersList();
+        },
+        (error) => {
+          this.messageService.add({ severity: 'error', detail: error.message});
+        });
+      }
+    } else{
+      if (this.user.name == '' || this.user.email == '' || this.user.gender == '' || this.user.status == '') {
+        this.messageService.add({ severity: 'warn', detail: 'Enter all User details'});      }
+      else {
+        this.userServiceService.updateUser(this.user).subscribe((result) => {
+          console.log("user updated", result);
+          this.showAddForm = false;
+          this.messageService.add({ severity: 'success', detail: 'User Updated'});
+          this.getUsersList();
+        },
+        (error) => {
+          this.messageService.add({ severity: 'error', detail: error.message});
+        });
+      }      
+    }
+
+  }
+
+  cancel(){
+    this.showAddForm = false;
+  }
+
+  getUsersList(){
+    this.userServiceService.getUsers().subscribe((result) => {
+      console.log("res",result);
+      this.userData = result;
+    },(error) => {
+      console.log("error",error)
+    })
+  }
+
+  onClickEdit(user: User) {
+    this.user = {...user};
+    this.showAddForm = true;
+  }
+
+  onClickDelete(user: User) {
+    this.confirmationService.confirm({
+      message: 'Are you sure you want to delete user ' + user.name + '?',
+      header: 'Confirm',
+      icon: 'pi pi-exclamation-triangle',
+      accept: () => {
+        this.userServiceService.deleteUser(user.id).subscribe((result) => {
+          this.getUsersList();
+          console.log("delete successfull", result);
+          this.messageService.add({ severity: 'success', detail: 'User Deleted!'});
+        },
+        (error) => {
+          console.log(error);
+        });
+      }
+  });    
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
+import {CardModule} from 'primeng/card';
+import {TableModule} from 'primeng/table';
+import {ToastModule} from 'primeng/toast';
+import {DialogModule} from 'primeng/dialog';
+import {ConfirmDialogModule} from 'primeng/confirmdialog';
 
 @NgModule({
   declarations: [
@@ -10,7 +20,20 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    InputTextModule,
+    ButtonModule,
+    RouterModule,
+    CardModule,
+    TableModule,
+    ToastModule,
+    DialogModule,
+    ConfirmDialogModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
